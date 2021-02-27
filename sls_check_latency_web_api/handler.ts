011cd4f0535a0e6c6a41f990968aaad6c97cc8d6
Hiro Kojima <guitarsaiko@gmail.com>
2021-02-27 12:22:48 +0900

    Support multiple targets

--- sls_check_latency_web_api/handler.ts
+++ sls_check_latency_web_api/handler.ts
@@ -1,27 +1,35 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import 'source-map-support/register';
-import 'request'
+import 'request';
 import * as request from 'request-promise';
 
 export const latencyCheck: APIGatewayProxyHandler = async (event, _context) => {
-
     // TODO: edit below information
-    const targetInfo = {
-        name: 'WriteTargetName',
-        uri: 'http://www.google.com'
+    const targets = [
+        {
+            name: 'WriteTargetName',
+            uri: 'http://www.google.com',
+        },
+    ];
+
+    for (const target of targets) {
+        await request({ uri: target.uri, method: 'GET', time: true }, (err, resp) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+            console.log(`[${target.name}] Time: ${resp.timings.end}`);
+        });
     }
 
-    await request({uri: targetInfo.uri, method:'GET', time: true}, (err, resp) => {
-        if(err){
-            console.error(err);
-            return ;
-        }
-        console.log(`[${targetInfo.name}] Time: ${resp.timings.end}`)
-    });
-    
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!'}, null, 2),
-  };
-}
+    return {
+        statusCode: 200,
+        body: JSON.stringify(
+            {
+                message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
+            },
+            null,
+            2
+        ),
+    };
+};
